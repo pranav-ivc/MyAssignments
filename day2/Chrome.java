baa40935c4e0a71fe4958e8b00ca99b97795178b
pranav-ivc <pranav.ivc@gmail.com>
2024-09-10 17:10:29 +0530

    Add files via upload
    Week1 Day2

---
+++ day2/Chrome.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version=91.0f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortCutKey='c';
+		
+		System.out.println("The Version is :"+version+",  The Developer is: "+developer+",  The isBeta is :"+isBeta+",  The Release year is : "+releaseYear+",  The Short cut key is :"+shortCutKey);
+
+	}
+
+}
---
+++ day2/Firefox.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserVersion = 100.2f; 
+	    String browserName = "firefox"; 
+		boolean isVisible = true; 
+		int releaseYear = 1998;  
+		char browserLogo = 'f'; 
+		
+		System.out.println("Browser version :"+browserVersion+",  Browser Name :"+browserName+",  Is Visible :"+isVisible+",  Release Year :"+releaseYear+",  Browser Logo :"+browserLogo);
+	}
+
+}
